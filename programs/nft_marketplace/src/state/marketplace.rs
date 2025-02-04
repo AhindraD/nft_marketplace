91ec49e06f29da339204f8aaeb946b431d0f5530
Ahindra D <official.ahindra@gmail.com>
2025-02-05 03:48:49 +0530

    marketplace-state

---
+++ programs/nft_marketplace/src/state/marketplace.rs
@@ -0,0 +1,19 @@
+use anchor_lang::prelude::*;
+
+use crate::ANCHOR_DISCRIMINATOR;
+
+#[account]
+pub struct Marketplace {
+    pub admin: Pubkey,
+    pub fee: u16,
+
+    pub bump: u8,
+    pub treasury_bump: u8,
+    pub rewards_mint_bump: u8,
+
+    pub name: String,
+}
+
+impl Space for Marketplace {
+    const INIT_SPACE: usize = ANCHOR_DISCRIMINATOR + 32 + 2 + 1 + 1 + 1 + (4 + 32);
+}
